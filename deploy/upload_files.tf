109dcbd492aa660bdacc6aed5c4089063a107b8f
FANYING XU <108683706+xfy0012@users.noreply.github.com>
2025-04-19 21:25:07 +0100

    Refactor: separate infra and deploy, add GitHub Actions

--- deploy/upload_files.tf
+++ deploy/upload_files.tf
@@ -1,13 +1,13 @@
 resource "aws_s3_object" "index" {
-  bucket       = aws_s3_bucket.static_site.id
+  bucket       = "fanyingxu-portfolio-site-0419"  
   key          = "index.html"
-  source       = "site/index.html"
+  source       = "../site/index.html"             
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
-  bucket       = aws_s3_bucket.static_site.id
+  bucket       = "fanyingxu-portfolio-site-0419"
   key          = "error.html"
-  source       = "site/error.html"
+  source       = "../site/error.html"
   content_type = "text/html"
 }
